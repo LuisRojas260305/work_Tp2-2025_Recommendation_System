7d8786d20087fee4c796bfcff6a7675f8145031b
LuisRojas260305 <luisalexanderrojasguevara@gmail.com>
2025-06-11 12:03:03 -0400

    modifique el main para ver si puedo trabajar en el repo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
 
 int main(int argc, char** argv) {
+	return 0;
+	
+	cout <<"puto el que lo lea";
+	
 	return 0;
 }
